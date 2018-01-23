dc87338b88093e1c97ec46bb18592a527204a7ae
Cat Janowitz and Luke Bertram <cat+luke@>
2018-01-23 09:27:43 -0800

    add comment descriptions to album service methods

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -11,10 +11,12 @@ export class AlbumService {
     this.albums = database.list('albums');
   }
 
+  //return an array of all albums to marketplace component
   getAlbums() {
     return this.albums;
   }
 
+  //return a single album with the provided id
   getAlbumById(albumId: number){
     for (var i = 0; i < ALBUMS.length; i++) {
       if (ALBUMS[i].id === albumId) {
